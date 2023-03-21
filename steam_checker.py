345d2e2620bf86ec68898f506d0b26fcbd39378a
jamie <jamiehamiltonnn@gmail.com>
2023-03-21 21:33:03 +0000

    Update to use asyncio and ThreadPoolExecutor
    This commit refactors the existing code to improve performance by introducing concurrency using asyncio and ThreadPoolExecutor. The new implementation checks multiple vanity URLs simultaneously, allowing for faster processing of large lists of names. Additionally, minor adjustments have been made to the code structure for readability and maintainability.

--- steam_checker.py
+++ steam_checker.py
@@ -1,3 +1,5 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import requests
 
 fname = "imported_accounts.txt"
@@ -8,23 +10,32 @@ def file_len(fname):
     with open(fname) as f:
         return sum(1 for _ in f)
 
-def name_check():
-    with open(fname, "r") as f, requests.Session() as session:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            url = f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={api_key}&vanityurl={line}"
-            response = session.get(url).json()
+def name_check(line):
+    with requests.Session() as session:
+        url = f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={api_key}&vanityurl={line}"
+        response = session.get(url).json()
 
-            if response["response"]["success"] == 42:  # 42 indicates that the custom URL is available
-                print(f"{line} is available.\nexported to text file: {available}")
-                with open(available, "a") as available_file:
-                    available_file.write(line + "\n")
-            else:
-                print(f"{line} is unavailable")
+        if response["response"]["success"] == 42:  # 42 indicates that the custom URL is available
+            with open(available, "a") as available_file:
+                available_file.write(line + "\n")
+            return line
+        else:
+            return None
 
-def main():
-    name_check()
+async def main():
+    with open(fname, "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    print("checking names...\n")
+    with ThreadPoolExecutor() as executor:
+        tasks = [loop.run_in_executor(executor, name_check, line) for line in lines]
+        results = await asyncio.gather(*tasks)
+
+    available_names = [name for name in results if name]
+    print("available names:")
+    for name in available_names:
+        print(name)
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
