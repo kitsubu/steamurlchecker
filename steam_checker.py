0dc4eb774dd32da7a9d7e29cfdeabbdb6e062b06
jamie <jamiehamiltonnn@gmail.com>
2023-03-21 21:04:18 +0000

    Optimised code and implement Steam API for availability check
    Refactored code to use a session for multiple requests, improving performance
    Replaced manual web scraping with Steam API for checking custom URL availability
    Updated file_len function to use a more efficient one-liner
    Added api_key variable to store the Steam API key for requests
    Enclosed the main function call within a main() function for better readability and organisation

--- steam_checker.py
+++ steam_checker.py
@@ -2,31 +2,29 @@ import requests
 
 fname = "imported_accounts.txt"
 available = "available_accounts.txt"
+api_key = "your_api_key"  # replace with your Steam API key
 
 def file_len(fname):
-	with open(fname) as f:
-		i = 0
-		for i, l in enumerate(f):
-			pass
-	return i + 1
-
-lines = file_len(fname)
+    with open(fname) as f:
+        return sum(1 for _ in f)
 
 def name_check():
-	f = open(fname)
-	lines = f.readlines()
-	
-	with open(fname, "r") as f:
-		lines = f.readlines()
-		for line in lines:
-			page = requests.get("http://steamcommunity.com/id/{}".format(line)).text
-			if "The specified profile could not be found." in page:
-				print("{} is available.\nexported to text file: {}".format(line.strip(), available))
-				
-				with open(available, "a") as f:
-					f.write(line)
-				
-			else:
-				print("{} is unavailable".format(line.strip()))
+    with open(fname, "r") as f, requests.Session() as session:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            url = f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={api_key}&vanityurl={line}"
+            response = session.get(url).json()
 
-name_check()
+            if response["response"]["success"] == 42:  # 42 indicates that the custom URL is available
+                print(f"{line} is available.\nexported to text file: {available}")
+                with open(available, "a") as available_file:
+                    available_file.write(line + "\n")
+            else:
+                print(f"{line} is unavailable")
+
+def main():
+    name_check()
+
+if __name__ == "__main__":
+    main()
